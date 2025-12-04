fbe8f67235cb4cf74b145d7632b327d86422c014
ridoq <ridoq444@gmail.com>
2025-12-04 09:09:11 +0700

    Percobaan 2

--- Kafe24.java
+++ Kafe24.java
@@ -1,16 +1,11 @@
 public class Kafe24 {
-    // public static void Menu(){
-    //     System.out.println("===== MENU RESTO KAFE24 =====");
-    //     System.out.println("1. Kopi Hitam - Rp 15.000");
-    //     System.out.println("2. Cappuccino - Rp 20.000");
-    //     System.out.println("3. Latte - Rp 22.000");
-    //     System.out.println("4. Teh Tarik - Rp 12.000");
-    //     System.out.println("5. Roti Bakar - Rp 10.000");
-    //     System.out.println("6. Mie GOreng - Rp 18.000");
-    //     System.out.println("===============================");
-    // }
-    public static void main(String[] args) {
-        // Menu();
+    public static void Menu(String namaPelanggan, boolean isMember) {
+        System.out.println("Selamat datang, " + namaPelanggan + "!");
+
+        if (isMember) {
+            System.out.println("Anda adalah member. dapatkan diskon 10% untuk setiap pembelian!");
+        }
+
         System.out.println("===== MENU RESTO KAFE24 =====");
         System.out.println("1. Kopi Hitam - Rp 15.000");
         System.out.println("2. Cappuccino - Rp 20.000");
@@ -19,5 +14,9 @@ public class Kafe24 {
         System.out.println("5. Roti Bakar - Rp 10.000");
         System.out.println("6. Mie GOreng - Rp 18.000");
         System.out.println("===============================");
+        System.out.println("Silahkan pilih menu yang anda inginkan.");
+    }
+    public static void main(String[] args) {
+        Menu("Andi", true);
     }
 }
