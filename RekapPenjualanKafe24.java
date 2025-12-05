28f650fca3c1628e791fd3d4b08d4607d0a5728f
ridoq <ridoq444@gmail.com>
2025-12-05 20:05:10 +0700

    Tugas 3b

--- RekapPenjualanKafe24.java
+++ RekapPenjualanKafe24.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 public class RekapPenjualanKafe24 {
     //Fungsi untuk menginputkan data penjualan setiap menu untuk setiap hari 
     public static void IsianRekapPenjualan(int rekapPenjualan[][], String menu[], Scanner sc){
+        System.out.println("=".repeat(35 * 2));
         for (int i = 0; i < rekapPenjualan.length; i++) {
             System.out.println("Masukkan data penjualan untuk menu " + menu[i] + ":");
             for (int j = 0; j < rekapPenjualan[i].length; j++) {
@@ -13,20 +14,18 @@ public class RekapPenjualanKafe24 {
     
     // Fungsi untuk menampilkan seluruh data penjualan
     public static void TampilRekapPenjualan(int rekapPenjualan[][], String menu[]){
-        System.out.println("\nRekap Penjualan KAFE24:");
-        System.out.print("Menu\\Hari\t");
+        System.out.println("=".repeat(35 * 2));
+        System.out.println("Rekap Penjualan KAFE24:");
+        System.out.printf("%-20s","Menu\\Hari");
         for (int j = 0; j < rekapPenjualan[0].length; j++) {
-            System.out.print("Hari " + (j + 1) + "\t");
+            System.out.printf("%-10s", "Hari " + (j + 1));
         }
         System.out.println();
         System.out.println("-".repeat(35 * 2));
         for (int i = 0; i < rekapPenjualan.length; i++) {
-            System.out.print(menu[i] + "\t");
-            if (menu[i].equalsIgnoreCase("Kopi") || menu[i].equalsIgnoreCase("Teh")) {
-                System.out.print("\t");
-            }
+            System.out.printf("%-20s", menu[i]);
             for (int j = 0; j < rekapPenjualan[i].length; j++) {
-                System.out.print(rekapPenjualan[i][j] + "\t");
+                System.out.printf("%-10d", rekapPenjualan[i][j]);
             }
             System.out.println();
         }
@@ -59,15 +58,37 @@ public class RekapPenjualanKafe24 {
         return (double) totalPenjualanMenu / rekapPenjualan[indexMenu].length;
     }
 
+    //fungsi input nama menu
+    public static String[] InputNamaMenu(Scanner sc, int jmlMenu){
+        String menu[] = new String[jmlMenu];
+        System.out.println("MASUKKAN NAMA MENU KAFE:");
+        for (int i = 0; i < menu.length; i++) {
+            System.out.print("Menu ke-" + (i + 1) + ": ");
+            menu[i] = sc.nextLine();
+        }
+        return menu;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String menu[] = {"Kopi", "Teh", "Es Kelapa Muda", "Roti Bakar", "Gorengan"};
-        int rekapPenjualan[][] = new int[5][7]; // 5 menu, 7 hari
+
+        System.out.print("Masukkan Jumlah Hari: ");
+        int jmlHari = sc.nextInt();
+        sc.nextLine(); 
+        System.out.print("Masukkan jumlah menu: ");
+        int jmlMenu = sc.nextInt();
+        sc.nextLine(); 
+        System.out.println("=".repeat(35 * 2));
+        String menu[] = InputNamaMenu(sc, jmlMenu);
+        int rekapPenjualan[][] = new int[jmlMenu][jmlHari];
+
         IsianRekapPenjualan(rekapPenjualan, menu, sc);
         TampilRekapPenjualan(rekapPenjualan, menu);
+        
         String hasilMenuTerlaris = MenuTerlaris(rekapPenjualan, menu);
         System.out.println(hasilMenuTerlaris);
-        System.out.println("\nRata-rata penjualan setiap menu selama 7 hari:");
+        System.out.println("=".repeat(35 * 2));
+        System.out.println("Rata-rata penjualan setiap menu selama " + jmlHari + " hari:");
         for (int i = 0; i < menu.length; i++) {
             double rataRata = RataRataPenjualan(rekapPenjualan, i);
             System.out.printf("%-25s: %.2f item%n", menu[i], rataRata);
